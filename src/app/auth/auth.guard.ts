309e1bb7455cb4b92dc49c2a681c7389d25ce253
himanshu077 <himanshukumar077@gmail.com>
2024-03-11 17:29:34 +0530

    work on services and install packages

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      return this.router.createUrlTree(['/login']); // Navigate to the login page using UrlTree
+    }
+  }
 }
---
+++ src/app/chats/chats.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chats',
+  templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.css'], // Use styleUrls instead of styleUrl
+})
+export class ChatsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
