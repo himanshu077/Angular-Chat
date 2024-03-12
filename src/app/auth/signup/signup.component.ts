ba7cfc6d2be7d389cccb40b53006b6969910de8d
himanshu077 <himanshukumar077@gmail.com>
2024-03-12 11:23:39 +0530

    Work on Auth functionality with jwt token is implimented add route part is done

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,8 +2,7 @@
 import { Component } from '@angular/core';
 import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-
+import { AuthService } from '../auth.service';
 
 @Component({
  selector: 'app-signup',
@@ -11,43 +10,59 @@ import { Router } from '@angular/router';
  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  signupForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
- signupForm= new FormGroup({
-   username:new FormControl(''),
-   email:new FormControl (''),
-   password:new FormControl (''),
- 
- })
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+    
+  }
 
+  signup() {
+    if (this.signupForm.valid) {
+      const userData = {
+        username: this.signupForm.value.username,
+        email: this.signupForm.value.email,
+        password: this.signupForm.value.password,
+      };
 
- constructor(private formBuilder: FormBuilder,
-   private router: Router,
-
-   ) {}
-
-
- ngOnInit() {
-
-
-   this.signupForm = this.formBuilder.group({
-     username: ['', [Validators.required]],
-     email: ['', [Validators.required,Validators.email]],
-     password: ['', [Validators.required, Validators.minLength(6)]],
-   
-
-
-
-
-   });
- }
-
-
- signup() {
- 
-
-}
-
+      this.authService.register(userData).subscribe({
+        next: (response) => {
+          console.log('signup response:', response);
+          this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          console.error('signup error:', error);
+          if (error.status === 400) {
+            console.log(
+              'Username or email already exists. Please choose another one.'
+            );
+            console.log("user already exist");
+            
+          } else {
+            console.log("signup failed");
+            
+          }
+        },
+      });
+    } else {
+      console.log('Form is not valid');
+      // Handle the case where the form is not valid
+    }
+  }
 
  get username() {
    return this.signupForm.get('username');
